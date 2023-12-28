5d5318e52585e0740f6387cfa7122acc80fa9a49
Hurmatullah <hrasaa1@gmail.com>
2023-12-28 01:44:47 -0800

    Update file name

--- Gec/LT_model.py
+++
@@ -1,6 +0,0 @@
-import language_tool_python
-tool = language_tool_python.LanguageTool('en-US')
-
-def language_tool_detect(sentence):
-    corrected_sentence = tool.correct(sentence)
-    return corrected_sentence
